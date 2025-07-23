5f8624e71947e1855fc5186d7f00ce4377d903de
william vidal <will_21_94@hotmail.com>
2025-07-22 18:50:19 -0600

    Initial Commit

---
+++ Prueba Macroplay/Prueba_MacroplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_MacroplayApp.swift
+//  Prueba Macroplay
+//
+//  Created by William Vidal on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_MacroplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
